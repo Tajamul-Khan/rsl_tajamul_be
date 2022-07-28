50e75112dfb28ba85bf1a6fcbcaa1772d1671c12
TajamulKhan <tajamulkhan0@gmail.com>
2022-07-28 21:32:33 +0530

    first commit

---
+++ src/main/java/com/rsl_tajamul_be/rsl_tajamul_be/RslTajamulBeApplication.java
@@ -0,0 +1,13 @@
+package com.rsl_tajamul_be.rsl_tajamul_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RslTajamulBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RslTajamulBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsl_tajamul_be/rsl_tajamul_be/RslTajamulBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsl_tajamul_be.rsl_tajamul_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RslTajamulBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
